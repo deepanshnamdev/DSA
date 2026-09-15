6cc6b6de2902377dc9e2203b6ac5e6c3d9780d22
Deepansh <namdevdeepansh5@gmail.com>
2026-09-15 22:16:56 +0530

    Time: 0 ms (100.00%) | Memory: 44.6 MB (69.50%) - LeetSync

--- 31-next-permutation/next-permutation.java
+++ 31-next-permutation/next-permutation.java
@@ -1,26 +1,26 @@
 class Solution {
     public void nextPermutation(int[] nums) {
         int n=nums.length;
-        int small_elemet_fromright=-1;
+        int small_elemet_from_right=-1;
         for(int i=n-1;i>0;i--){
             if(nums[i]>nums[i-1]){
-                small_elemet_fromright=i-1;
+                small_elemet_from_right=i-1;
                 break;
             }
         }
-        int largest_element_form_small_elemet_from_right=small_elemet_fromright;
-        if(small_elemet_fromright>-1){
-            for(int j=n-1;j>small_elemet_fromright;j--){
-                if(nums[j]>nums[small_elemet_fromright]){
+        int largest_element_form_small_elemet_from_right=small_elemet_from_right;
+        if(small_elemet_from_right>-1){
+            for(int j=n-1;j>small_elemet_from_right;j--){
+                if(nums[j]>nums[small_elemet_from_right]){
                     largest_element_form_small_elemet_from_right=j;
                     break;
                 }
             }
-            int temp=nums[small_elemet_fromright];
-            nums[small_elemet_fromright]=nums[largest_element_form_small_elemet_from_right];
+            int temp=nums[small_elemet_from_right];
+            nums[small_elemet_from_right]=nums[largest_element_form_small_elemet_from_right];
             nums[largest_element_form_small_elemet_from_right]=temp;
         }
-        int left=small_elemet_fromright+1;
+        int left=small_elemet_from_right+1;
         int right=n-1;
         while(left<right){
             int temp= nums[left];
